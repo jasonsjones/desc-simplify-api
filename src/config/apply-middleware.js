a88b6b971a18ebc3904901eaa08129b50221c6ed
Jason Jones <jsjones96@gmail.com>
2018-09-25 15:55:37 -0700

    Add cors middleware

--- src/config/apply-middleware.js
+++ src/config/apply-middleware.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import config from './config';
 
@@ -9,6 +10,7 @@ export default (app, passport) => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(passport.initialize());
+    app.use(cors());
 
     if (config.env !== 'testing') {
         app.use(morgan('dev'));
