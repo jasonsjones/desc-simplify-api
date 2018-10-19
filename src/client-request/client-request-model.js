ceb1671ac84ea432df118d1e32370b3f2c8f76cd
Jason Jones <jsjones96@gmail.com>
2018-10-19 15:19:26 -0700

    feat: add client requiest data model

---
+++ src/client-request/client-request-model.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const clientRequestSchema = new Schema(
+    {
+        clientId: { type: String, required: true },
+        requestedBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+        items: [{ type: String }]
+        //items: [{ type: Schema.Types.ObjectId, ref: 'Item' }]
+    },
+    { timestamps: true }
+);
+
+const ClientRequest = mongoose.model('ClientRequest', clientRequestSchema);
+
+export default ClientRequest;
---
+++ src/client-request/client-request-model.u-test.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+import { expect } from 'chai';
+import ClientRequest from './client-request-model';
+
+mongoose.Promise = global.Promise;
+
+describe('Client request model', () => {
+    describe('field validations', () => {
+        it('is valid when all required fields are provided', done => {
+            const request = new ClientRequest({
+                clientId: '12345678',
+                requestedBy: '5bb69cb1322fdf5690edfc0b',
+                items: ['5bca0257b2f5ce2c899c08d3', '5bca0732ef3f813295e10966']
+            });
+
+            request.validate(err => {
+                expect(err).to.not.exist;
+                done();
+            });
+        });
+
+        it('is invalid if the clientId field is empty', done => {
+            const request = new ClientRequest({
+                requestedBy: '5bb69cb1322fdf5690edfc0b',
+                items: ['5bca0257b2f5ce2c899c08d3', '5bca0732ef3f813295e10966']
+            });
+
+            request.validate(err => {
+                expect(err.errors['clientId']).to.exist;
+                expect(err.name).to.equal('ValidationError');
+                done();
+            });
+        });
+
+        it('is invalid if the requestedBy field is empty', done => {
+            const request = new ClientRequest({
+                clientId: '12345678',
+                items: ['5bca0257b2f5ce2c899c08d3', '5bca0732ef3f813295e10966']
+            });
+
+            request.validate(err => {
+                expect(err.errors['requestedBy']).to.exist;
+                expect(err.name).to.equal('ValidationError');
+                done();
+            });
+        });
+    });
+});
