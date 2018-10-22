4c3c3e97e1e67c5ef065aff42c9200a34fc163e5
Jason Jones <jsjones96@gmail.com>
2018-10-22 11:09:57 -0700

    feat: add personal hygiene item data model

---
+++ src/item/categories/personalhygiene-model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+import Item from '../baseitem-model';
+
+const Schema = mongoose.Schema;
+
+const HYGIENE_ITEMS = [
+    'soap',
+    'shampoo',
+    'conditioner',
+    'brush/comb',
+    'toothbrush',
+    'toothpaste',
+    'floss',
+    'feminine pad',
+    'tampons',
+    'toilet paper'
+];
+
+const options = {
+    discriminatorKey: 'itemCategory',
+    timestamps: true
+};
+
+const personaHygieneSchema = new Schema(
+    {
+        name: { type: String, enum: HYGIENE_ITEMS, required: true }
+    },
+    options
+);
+
+const PersonalHygiene = Item.discriminator('PersonalHygiene', personaHygieneSchema);
+
+export default PersonalHygiene;
