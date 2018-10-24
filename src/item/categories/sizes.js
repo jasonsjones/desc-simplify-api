13502d985f60109a04a1ecc91a846734a88cbf15
Jason Jones <jsjones96@gmail.com>
2018-10-24 15:23:55 -0700

    feat: add clothing sizes per gender

---
+++ src/item/categories/sizes.js
@@ -0,0 +1,95 @@
+export const menShirtCoatSizes = [
+    'XS (32)',
+    'S (34-36)',
+    'M (38-40)',
+    'L (42-44)',
+    'XL (46)',
+    'XXL (48)',
+    'XXXL (50)'
+];
+
+export const menPantSizes = [
+    'XS (waist 28)',
+    'S (waist 30)',
+    'M (waist 32)',
+    'L (waist 34)',
+    'XL (waist 36)',
+    'XXL (waist 38)',
+    'XXXL (waist 40+)'
+];
+
+export const menShoeSizes = [
+    '6',
+    '6.5',
+    '7',
+    '7.5',
+    '8',
+    '8.5',
+    '9',
+    '9.5',
+    '10',
+    '10.5',
+    '11',
+    '11.5',
+    '12',
+    '12.5',
+    '13',
+    '13.5',
+    '14',
+    '14.5',
+    '15',
+    '15.5',
+    '16'
+];
+
+export const womenShirtCoatSizes = [
+    'XS (0)',
+    'S (2-4)',
+    'M (6-8)',
+    'L (10-12)',
+    'XL (14-16)',
+    'XXL (18-20)',
+    'XXXL (22-24)'
+];
+
+export const womenPantSizes = [
+    'XS (25-26)',
+    'S (27-29)',
+    'M (30-32)',
+    'L (33-37)',
+    'XL (38-41)',
+    'XXL (42-46)',
+    'XXXL (47+)'
+];
+
+export const womenShoeSizes = [
+    '4',
+    '4.5',
+    '5',
+    '5.5',
+    '6',
+    '6.5',
+    '7',
+    '7.5',
+    '8',
+    '8.5',
+    '9',
+    '9.5',
+    '10',
+    '10.5',
+    '11',
+    '11.5',
+    '12'
+];
+
+export const socksAndUnderwearSizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'];
+
+export const womenBraSizes = [
+    'S (32-34, A-B)',
+    'M (34-37, B-C)',
+    'L (37-41, C-D)',
+    'XL (40-43 D-E)',
+    'XXXL (43+ D+)'
+];
+
+// scarf & hat sizes are N/A
