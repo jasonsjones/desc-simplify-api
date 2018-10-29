5136a53d9aa0d19492c6866f4ea1fd10b9b99156
Jason Jones <jsjones96@gmail.com>
2018-10-29 13:16:43 -0700

    feat: add item related routes

--- src/config/attach-routers.js
+++ src/config/attach-routers.js
@@ -1,9 +1,11 @@
 import indexRouter from '../index/index-router';
 import userRouter from '../user/user-router';
 import authRouter from '../auth/auth-router';
+import itemRouter from '../item/item-router';
 
 export default (app, passport) => {
     app.use('/', indexRouter());
     app.use('/api/users', userRouter());
     app.use('/api/auth', authRouter(passport));
+    app.use('/api/items', itemRouter());
 };
---
+++ src/item/item-router.js
@@ -0,0 +1,81 @@
+import express from 'express';
+import * as ItemController from './item-controller';
+
+const handleError = response => {
+    return err =>
+        response.json({
+            success: false,
+            message: err.message,
+            error: err
+        });
+};
+
+export default () => {
+    let itemRouter = express.Router();
+
+    itemRouter
+        .route('/')
+        .get((req, res) => {
+            ItemController.getItems()
+                .then(items =>
+                    res.json({
+                        success: true,
+                        message: 'items fetched',
+                        payload: {
+                            items
+                        }
+                    })
+                )
+                .catch(handleError(res));
+        })
+        .post((req, res, next) => {
+            ItemController.createItem(req.body)
+                .then(item =>
+                    res.json({
+                        success: true,
+                        message: 'item created',
+                        payload: {
+                            item
+                        }
+                    })
+                )
+                .catch(handleError(res));
+        });
+
+    itemRouter
+        .route('/:id([0-9a-zA-Z]{24})')
+        .get((req, res) => {
+            ItemController.getItem(req.params.id)
+                .then(item =>
+                    res.json({
+                        success: true,
+                        message: 'item fetched',
+                        payload: { item }
+                    })
+                )
+                .catch(handleError(res));
+        })
+        .put((req, res) => {
+            ItemController.updateItem(req.params.id, req.body)
+                .then(item =>
+                    res.json({
+                        success: true,
+                        message: 'item updated',
+                        payload: { item }
+                    })
+                )
+                .catch(handleError(res));
+        })
+        .delete((req, res) => {
+            ItemController.deleteItem(req.params.id)
+                .then(item =>
+                    res.json({
+                        success: true,
+                        message: 'item deleted',
+                        payload: { item }
+                    })
+                )
+                .catch(handleError(res));
+        });
+    return itemRouter;
+};
