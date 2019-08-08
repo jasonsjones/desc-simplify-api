dba722b2d6cc19f24d5b02d8c73f00608147dab1
Jason S. Jones <jsjones96@gmail.com>
2019-08-07 19:56:29 -0700

    feat: add processToken middleware

--- src/config/apply-middleware.js
+++ src/config/apply-middleware.js
@@ -1,9 +1,46 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import cors from 'cors';
 
 import config from './config';
+import { verifyToken } from '../auth/auth-utils';
+
+const getTokens = req => {
+    let bearerToken = null;
+    if (req.headers.authorization) {
+        bearerToken = req.headers.authorization.split(' ')[1];
+    }
+
+    const token = bearerToken || req.cookies['access-token'];
+    const refreshToken = req.cookies['refresh-token'];
+
+    return {
+        token,
+        refreshToken
+    };
+};
+
+const processToken = (req, res, next) => {
+    const { token } = getTokens(req);
+    if (token) {
+        try {
+            const decoded = verifyToken(token);
+            if (decoded) {
+                req.user = {
+                    id: decoded.sub,
+                    email: decoded.email
+                };
+            }
+        } catch (err) {
+            req.user = null;
+        }
+    } else {
+        req.user = null;
+    }
+    next();
+};
 
 const corsOptions = {
     origin: 'http://localhost:4200',
@@ -14,8 +51,10 @@ export default (app, passport) => {
     app.use(express.static('public'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cookieParser());
     app.use(cors(corsOptions));
     app.use(passport.initialize());
+    app.use(processToken);
 
     if (config.env === 'development') {
         app.use(morgan('dev'));
